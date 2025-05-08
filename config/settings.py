20e1596891f9d238d147a0404c2f3716d0d64a13
Abdurazzoq Abdusalomov <rayyan.dev@mail.ru>
2025-05-08 05:31:33 +0500

    static fayllar to'g'rilandi

--- config/settings.py
+++ config/settings.py
@@ -109,10 +109,10 @@ STATIC_URL = "static/"
 
 
 if DEBUG:
-    STATIC_ROOT = BASE_DIR / "static"
+    STATICFILES_DIRS = [BASE_DIR / "static"]
 
 else:
-    STATICFILES_DIRS = [BASE_DIR / "static"]
+    STATIC_ROOT = BASE_DIR / "static"
 
 MEDIA_ROOT = BASE_DIR / "media"
 MEDIA_URL = "/media/"
